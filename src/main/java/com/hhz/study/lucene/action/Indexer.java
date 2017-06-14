f9ae8235cbe4d5fbe2c56c8b46ff75cfa9032b5b
hejiafu <hejiafu@secoo.com>
2017-06-14 17:38:09 +0800

    update version

--- src/main/java/com/hhz/study/lucene/action/Indexer.java
+++ src/main/java/com/hhz/study/lucene/action/Indexer.java
@@ -1,79 +1,37 @@
 package com.hhz.study.lucene.action;
 
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileReader;
-import java.io.IOException;
-
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.StringField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
 
-import com.hhz.study.util.Configuration;
+import java.nio.file.Paths;
 
 public class Indexer {
 	/**
 	 * 给data目录建索引，建好后放在index目录中 lucene实战p20
 	 */
 	public static void main(String[] args) throws Exception {
-		String indexDir = Configuration.getProperty("lucene.indexDir");
-		String dataDir = Configuration.getProperty("lucene.dataDir");
-		Indexer indexer = new Indexer(indexDir);
-		int numIndexed = indexer.index(dataDir, new TextFilesFilter());
-		indexer.close();
-		System.out.println("indexing " + numIndexed);
-	}
-
-	private IndexWriter writer;
-
-	public Indexer(String indexDir) throws IOException {
-		Directory dir = FSDirectory.open(new File(indexDir));
-		IndexWriterConfig config = new IndexWriterConfig(
-				Version.LUCENE_CURRENT, new StandardAnalyzer());
-		writer = new IndexWriter(dir, config);
-	}
-
-	public void close() throws IOException {
-		writer.close();
-	}
-
-	public int index(String dataDir, FileFilter filter) throws Exception {
-		File[] files = new File(dataDir).listFiles();
-		for (File f : files) {
-			if (!f.isDirectory() && !f.isHidden() && f.exists() && f.canRead()
-					&& (filter == null || filter.accept(f))) {
-				indexFile(f);
-			}
-		}
-		return writer.numDocs();
-	}
-
-	private static class TextFilesFilter implements FileFilter {
-
-		@Override
-		public boolean accept(File path) {
-			return path.getName().toLowerCase().endsWith(".txt");
-		}
-	}
-
-	private Document getDocument(File f) throws Exception {
-		Document doc = new Document();
-		doc.add(new Field("contents", new FileReader(f)));
-		doc.add(new Field("filename", f.getName(), Field.Store.YES,
-				Field.Index.NOT_ANALYZED));
-		doc.add(new Field("fullpath", f.getCanonicalPath(), Field.Store.YES,
-				Field.Index.NOT_ANALYZED));
-		return doc;
-	}
-
-	private void indexFile(File f) throws Exception {
-		System.out.println("indexing " + f.getCanonicalPath());
-		Document doc = getDocument(f);
-		writer.addDocument(doc);
+        Directory dir = FSDirectory.open(Paths.get("searcher"));
+        Analyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE_OR_APPEND);
+        config.setOpenMode(IndexWriterConfig.OpenMode.CREATE);
+        IndexWriter iWriter = new IndexWriter(dir, config);
+        Document doc = new Document();
+        String name = "This is the text to be indexed.";
+        doc.add(new StringField("name", name, Field.Store.YES));
+        doc.add(new StringField("age", "20", Field.Store.YES));
+        doc.add(new IntPoint("grade", 4));
+        iWriter.addDocument(doc);
+        iWriter.commit();
+        iWriter.close();
+        dir.close();
 	}
 }
--- src/main/java/com/hhz/study/lucene/action/Searcher.java
+++ src/main/java/com/hhz/study/lucene/action/Searcher.java
@@ -1,46 +1,39 @@
 package com.hhz.study.lucene.action;
 
-import java.io.File;
-import java.io.IOException;
-
+import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
 
-import com.google.common.base.Stopwatch;
-import com.hhz.study.util.Configuration;
-import com.hhz.study.util.Console;
+import java.io.IOException;
+import java.nio.file.Paths;
 
 public class Searcher {
 	public static void main(String[] args) throws IOException, ParseException {
-		String indexDir = Configuration.getProperty("lucene.indexDir");
-		String q = "solr";
-		search(indexDir, q);
-	}
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get("searcher")));
+        IndexSearcher searcher = new IndexSearcher(reader);
+//		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, "name",
+//				analyzer);
+//		Query query = parser.parse("text");
+        Analyzer analyzer = new StandardAnalyzer();
+        QueryParser parser = new QueryParser("age", analyzer);
+        Query query = parser.parse("age:20");
+        TopDocs tfd = searcher.search(query, 1);
+        ScoreDoc[] hits = tfd.scoreDocs;
+        for (int i = 0; i < hits.length; i++) {
+            Document hitDoc = searcher.doc(hits[i].doc);
+            System.out.println(hitDoc.get("name"));
+            System.out.println(hitDoc.getFields());
+        }
+        reader.close();
+    }
 
-	public static void search(String indexDir, String q) throws IOException,
-			ParseException {
-		Directory dir = FSDirectory.open(new File(indexDir));
-		IndexSearcher is = new IndexSearcher(DirectoryReader.open(dir));
-		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT,
-				"contents", new StandardAnalyzer(Version.LUCENE_CURRENT));
-		Query query = parser.parse(q);
-		Stopwatch sw = Stopwatch.createStarted();
-		TopDocs hits = is.search(query, 10);
-		sw.stop();
-		Console.writeTime("命中" + hits.totalHits, sw);
-		for (ScoreDoc scoreDoc : hits.scoreDocs) {
-			Document doc = is.doc(scoreDoc.doc);
-			System.out.println(doc.get("fullpath"));
-		}
-	}
 }
